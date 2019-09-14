708a31839b046ba208f0926e9eeaa2dbe8b40396
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-09-14 01:13:50 -0500

    Cleanup

--- src/app/components/App.svelte
+++ src/app/components/App.svelte
@@ -3,12 +3,9 @@
 
   import NotFound from './pages/NotFound.svelte';
   import Home from './pages/Home.svelte';
-
-  const base = document.querySelector('base');
-  const baseHref = base && base.href;
 </script>
 
-<Router path={baseHref}>
+<Router>
   <nav>
     <ul>
       <li>
